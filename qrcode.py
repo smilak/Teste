ab100846f0222bff87df33313c96dbf193018cc7
smilak <smilakneto@gmail.com>
2024-10-28 15:40:13 -0300

    teste

---
+++ qrcode.py
@@ -0,0 +1,14 @@
+import segno
+
+# Dados que você quer codificar no QR Code
+data = "https://www.google.com"
+
+# Gera o QR Code
+qr = segno.make(data)
+
+# Salva a imagem em um arquivo
+
+qr.save("imagens_qrcode/qrcode.png", scale=10, dark='blue', light='white')
+
+print("QR Code gerado e salvo como 'qrcode.png'")
+
